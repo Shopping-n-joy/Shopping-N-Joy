f3725097c8710723fd3196515c2a13eaac0c2bb5
Dennis12ax <laa1012@naver.com>
2023-05-01 20:11:07 +0900

    test for setup 2

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = 'snj_lib'
-    version = '1.0.0'
-    url = 'https://github.com/Shopping-n-joy/Shopping-N-Joy.git'
-    author = 'Dennis12ax'
-    author_email = "test"
-    license = "Dennis12ax"
-    packages = ['snj_lib']
+    name = 'snj_lib',
+    version = '1.0.0',
+    description = 'snj pip install library',
+    url = 'https://github.com/Shopping-n-joy/Shopping-N-Joy.git',
+    author = 'Dennis12ax',
+    author_email = "test",
+    license = "Dennis12ax",
+    packages = ['snj_lib'],
     zip_safe = False,
     install_requires = [
         'openai == 0.27.5'
